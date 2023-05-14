65ea5d04de1e5505d047c01629745d45055a0433
김용민 <dydals3440@gmail.com>
2023-05-14 20:57:29 +0900

    feat: isAdmin 구분, firebase RealTime DB 이용

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,6 +6,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
+import { getDatabase, ref, child, get } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -18,10 +19,14 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const dbRef = ref(getDatabase());
 
 // async는 항상 promise를 반환하니, .then이나 await을 통해서 실제값을 얻어야하고, 얻은 값들을 return 해주어야 user정보는 null 정보로 상태를 초기화 해 줄 수 있다.
+// user정보는 auth에 담겨져있음.
 export function login() {
   signInWithPopup(auth, provider).catch(console.error);
+  // user정보 확인
+  console.log(auth);
 }
 
 export function logout() {
@@ -29,9 +34,30 @@ export function logout() {
 }
 // Header에서 선언할 수 있지만, 이런 모든 firebase로직은 파일 안에서만 관리하고, 함수로추출
 export function onUserStateChange(callback) {
-  onAuthStateChanged(auth, (user) => {
+  onAuthStateChanged(auth, async (user) => {
     if (user) {
-      callback(user);
+      // 사용자가 있는 경우에 adminUser를 호출
+      const updatedUser = user ? await adminUser(user) : null;
+      callback(updatedUser);
     }
   });
+}
+
+// read DB
+async function adminUser(user) {
+  return get(child(dbRef, 'admins'))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val();
+        console.log('admins', admins);
+        const isAdmin = admins.includes(user.uid);
+        console.log('isAdmin', isAdmin);
+        return { ...user, isAdmin };
+      } else {
+        return user;
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
