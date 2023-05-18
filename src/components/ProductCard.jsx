97f3d3e44e5c97b25237ad0fc42bf7dbb820b652
김용민 <dydals3440@gmail.com>
2023-05-18 22:06:59 +0900

    feat: product Detail Page 구조 잡기

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 export const ProductCard = ({
+  // product를 구조분해할당을 했기 때문에 product에 접근을 못하므로 한번더 인자로 정리해줌
+  product,
   product: { id, image, title, category, price },
 }) => {
+  const navigate = useNavigate();
   return (
-    <li className='rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105'>
+    <li
+      onClick={() => {
+        // product : product => product라고만 작성해도됨
+        navigate(`/products/${id}`, { state: { product: product } });
+      }}
+      className='rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105'
+    >
       <img className='w-full' src={image} alt={title} />
       <div className='mt-2 px-2 text-lg flex justify-between items-center'>
         <h3 className='truncate'>{title}</h3>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Button } from '../components/ui/Button';
 
 export const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  };
+  const handleClick = () => {
+    // 장바구니에 추가하면 됩니다.
+  };
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState(options && options[0]);
+  return (
+    <section>
+      <p>{category}</p>
+      <img src={image} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <p>₩{price}</p>
+        <p>옵션: </p>
+        <select onChange={handleSelect} value={selected}>
+          {options &&
+            options.map((option, index) => (
+              <option key={index}>{option}</option>
+            ))}
+        </select>
+        <Button text='장바구니에 추가' onClick={handleClick} />
+      </div>
+    </section>
+  );
 };
